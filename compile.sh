4e1814025f37489118daf522dd5ee164be58d7ae
nakagawa <nakagawa@nakagawataikainoMacBook-Pro.local>
2017-01-13 14:15:16 +0900

    初稿

--- compile.sh
+++ compile.sh
@@ -1,7 +1,7 @@
-platex master_thesis.tex;
-biber --bblencoding=utf8 -u -U --output_safechars master_thesis.aux ;
-platex master_thesis.tex;
-platex master_thesis.tex;
+platex grad_thesis.tex;
+biber --bblencoding=utf8 -u -U --output_safechars grad_thesis.aux ;
+platex grad_thesis.tex;
+platex grad_thesis.tex;
 
-dvipdfmx -p a4 master_thesis.dvi;
-open -g master_thesis.pdf;
+dvipdfmx -p a4 grad_thesis.dvi;
+open -g grad_thesis.pdf;
